defc19d3ad54dcd2f1dd096963241909669625cf
wxd <wangxingda@jd.com>
2024-12-09 09:35:37 +0000

    update

--- cloud/start.sh
+++ cloud/start.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-CPUS=`cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us` && [ -n $CPUS ] && [ $CPUS -gt 0 ] && CPUS=`expr $CPUS / 100000` && echo $CPUS && export OMP_NUM_THREADS=$CPUS
+if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ]; then
+    CPUS=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us) && [ -n $CPUS ] && [ $CPUS -gt 0 ] && CPUS=$(expr $CPUS / 100000) && echo $CPUS && export OMP_NUM_THREADS=$CPUS
+fi
 
 if [ -z "$1" ]; then
     echo "start args is empty"
